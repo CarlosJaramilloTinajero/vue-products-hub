01bedf0425b6941b5b7df30cb37de6bb7daf2027
CarlosJaramilloTinajero <75448585+CarlosJaramilloTinajero@users.noreply.github.com>
2024-04-15 17:00:45 -0600

    Agregue el link para el productos en la lista de los favoritos

--- src/components/modules/wishlist/WishListProduct.vue
+++ src/components/modules/wishlist/WishListProduct.vue
@@ -28,23 +28,28 @@ export default defineComponent({
 
 <template>
     <div class="product">
-        <section class="img-container">
-            <img :src="product.img_path" alt="image product">
-        </section>
+        <router-link style="text-decoration: none !important;" :to="`/product/${product.slug}`">
+            <section class="img-container">
+                <img :src="product.img_path" alt="image product">
+            </section>
+        </router-link>
         <section class="info-product-container">
-            <p class="name">{{ !lesswords ? product.name : product.name.slice(0, 40) }}<span
-                    v-show="lesswords && product.name.length >= 40">...</span></p>
-            <p class="sku">{{ product.sku }}</p>
-            <p class="price"><strong>$ {{ product.price }}</strong><span class="discount" v-if="randomBoolean()">{{
-                randomNumber(20) }}%
-                    OFF</span></p>
+            <router-link style="text-decoration: none !important;" :to="`/product/${product.slug}`">
+                <p class="name">{{ !lesswords ? product.name : product.name.slice(0, 40) }}<span
+                        v-show="lesswords && product.name.length >= 40">...</span></p>
+                <p class="sku">{{ product.sku }}</p>
+                <p class="price"><strong>$ {{ product.price }}</strong><span class="discount" v-if="randomBoolean()">{{
+                    randomNumber(20) }}%
+                        OFF</span></p>
+            </router-link>
+
             <div class="options-down">
                 <button class="option" v-on:click="removeProduct()">Eliminar</button>
             </div>
 
             <!-- <div class="option-top">
-                        
-                    </div> -->
+                            
+                        </div> -->
         </section>
     </div>
 </template>
